a8e5a1d9b6ab4a29162da6ae74da3e7e10790c61
rjb-itdev <42055646+rjb-itdev@users.noreply.github.com>
2018-08-18 14:10:09 +1200

    Commit testing

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,12 @@
-// Create the module              // Dependencies go in where the [] are
-var app = angular
-          .module('myModule', [])
-          .controller("myController", function($scope, $http){
-
-            $http.get("data.json").then(function(response) {
-              $scope.myData = response.data[1];
-            });
-      });
+var app=angular.module("app",["ngRoute"])
+.config(function($routeProvider, $locationProvider){
+  $routeProvider
+  .when("/book",{
+    controller: "bookController",
+    templateUrl:"Templates/book.html"
+  })
+  $locationProvider .html5Mode(true);
+})
+.controller("bookController",function($scope){
+  $scope.message="Hi Im in books";
+});
