3623684615f1d4759b6fb341230f18aef4d10cd8
rjb-itdev <42055646+rjb-itdev@users.noreply.github.com>
2018-08-18 14:20:54 +1200

    tt

---
+++ app.js
@@ -0,0 +1,12 @@
+var app=angular.module("app",["ngRoute"])
+.config(function($routeProvider, $locationProvider){
+  $routeProvider
+  .when("/book",{
+    controller: "bookController",
+    templateUrl:"Templates/book.html"
+  })
+  $locationProvider .html5Mode(true);
+})
+.controller("bookController",function($scope){
+  $scope.message="Hi Im in books";
+});
